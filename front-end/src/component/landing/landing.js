550c1173dc55c8e88cf84f63b38f527164958227
Tom <tnorth93@gmail.com>
2018-10-31 16:58:01 -0700

    landing fleshed out

--- front-end/src/component/landing/landing.js
+++ front-end/src/component/landing/landing.js
@@ -8,8 +8,68 @@ import * as routes from '../../routes';
 import * as authActions from '../../action/auth';
 
 class Landing extends React.Component {
-  handle
+  handleLogin = (user) => {
+    return this.props.pDoLogin(user)
+      .then(() => {
+        this.props.history.push(routes.DASHBOARD);
+      })
+      .catch(console.error);
+  };
+
+  handleSignup = (user) => {
+    return this.props.pDoSignUp(user)
+      .then(() => {
+        this.props.history.push(routes.DASHBOARD);
+      })
+      .catch(console.error);
+  };
+
+  render() {
+    const rootJSX = <div>
+      <h2> Welcome to ApiDND! </h2>
+      <Link to='/signup'>Sign up to play ApiDnD</Link>
+      <Link to='/login'>Login to an existing account</Link>
+    </div>;
+
+    const signUpJSX = <div>
+      <h2> Sign Up!</h2>
+      <AuthForm type='signup' onComplete={this.handleSignup}/>
+      <p> Already have an account? </p>
+      <Link to='/login'> Login </Link>
+    </div>;
+
+    const loginJSX = <div>
+      <h2> Login to ApiDnD</h2>
+      <AuthForm type='login' onComplete={this.handleLogin}/>
+      <p> Do not have an account? </p>
+      <Link to='/signup'>Sign up</Link>
+    </div>;
+
+    const { location } = this.props;
+
+    return (
+      <nav>
+        { location.pathname === routes.ROOT ? rootJSX : undefined }
+        { location.pathname === routes.SIGNUP_FRONTEND ? signUpJSX : undefined }
+        { location.pathname === routes.LOGIN ? loginJSX : undefined }
+      </nav>
+    );
+  }
 }
 
+const mapStateToProps = state => ({
+  token: state.token,
+});
 
-export default Landing;
+const mapDispatchToProps = dispatch => ({
+  pDoSignUp: user => dispatch(authActions.signupRequest(user)),
+  pDoLogin: user => dispatch(authActions.loginRequest(user)),
+});
+
+Landing.propTypes = {
+  location: PropTypes.object,
+  pDoSignUp: PropTypes.func,
+  pDoLogin: PropTypes.func,
+};
+
+export default connect(mapStateToProps,mapDispatchToProps)(Landing);
